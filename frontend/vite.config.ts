036e730023f36b5ea3623e8ce4cdfb7c38e9fe77
Ricardo <ricardosantos20193@gmail.com>
2026-04-05 16:30:29 -0300

    corrigindo seguranca do vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-      "/media": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+    allowedHosts: true,
+    host: '0.0.0.0',
+    port: 80
+  }
+})
